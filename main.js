d9c295126046065ca105e772365dab0c6ef6ae9b
Alinilimaa <Alini.lima@escola.pr.gov.br>
2025-08-20 21:46:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-12-30T00:00:00");
-const tempoObjetivo2 = new Date("2026-01-01T00:00:00");
-const tempoObjetivo3 = new Date("2026-02-03T00:00:00");
+const tempoObjetivo2 = new Date("2026-01-10T00:00:00");
+const tempoObjetivo3 = new Date("2026-02-20T00:00:00");
 const tempoObjetivo4 = new Date("2026-01-10T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
